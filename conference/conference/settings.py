fe5612dd47e7a3dbcdfee0c3bab82aa8163eba82
FKC 117 <fazlul.karim117@gmail.com>
2025-12-05 17:57:17 +0600

    login logout made available global

--- conference/conference/settings.py
+++ conference/conference/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Add registration user_profile context
+                'registration.context_processors.user_profile',
             ],
         },
     },
